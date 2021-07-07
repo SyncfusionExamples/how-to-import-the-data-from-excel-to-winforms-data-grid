092b7e4399660e537361be1dcd809ec9302f7049
vijayarasan <58214164+vijayarasan@users.noreply.github.com>
2021-07-07 13:22:11 +0530

    Sample Added
    Sample Added

---
+++ Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Syncfusion.XlsIO;
+using System.Data;
+
+namespace SfDataGridDemo
+{
+    /// <summary>
+    /// Summary description for Form1.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        ViewModel viewModel;
+        public Form1()
+        {
+            InitializeComponent();
+            viewModel = new ViewModel();
+        }
+
+        private void btnImportData_Click(object sender, EventArgs e)
+        {
+            ExcelEngine excelEngine = new ExcelEngine();
+
+            IApplication application = excelEngine.Excel;
+
+            application.DefaultVersion = ExcelVersion.Excel2013;
+
+            IWorkbook workbook = application.Workbooks.Create(1);
+
+            IWorksheet worksheet = workbook.Worksheets[0];
+
+            //Import the data to worksheet.
+            ObservableCollection<OrderInfo> reports = viewModel.Generate();
+            worksheet.ImportData(reports, 1, 1, true);
+
+            // Read data from the worksheet and Export to the DataTable.
+
+            DataTable customersTable = worksheet.ExportDataTable(1, 1, 15, 5, ExcelExportDataTableOptions.ColumnNames);
+
+            this.sfDataGrid.DataSource = customersTable;
+
+            workbook.Close();
+
+            excelEngine.Dispose();
+        }
+    }
+
+    public class OrderInfo : INotifyPropertyChanged
+    {
+        decimal? orderID;
+        string customerId;
+        string country;
+        string customerName;
+        string shippingCity;
+        bool isShipped;
+
+        public OrderInfo()
+        {
+
+        }
+
+        public decimal? OrderID
+        {
+            get { return orderID; }
+            set { orderID = value; this.OnPropertyChanged("OrderID"); }
+        }
+
+        public string CustomerID
+        {
+            get { return customerId; }
+            set { customerId = value; this.OnPropertyChanged("CustomerID"); }
+        }
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; this.OnPropertyChanged("CustomerName"); }
+        }
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; this.OnPropertyChanged("Country"); }
+        }
+
+        public string ShipCity
+        {
+            get { return shippingCity; }
+            set { shippingCity = value; this.OnPropertyChanged("ShipCity"); }
+        }
+
+        public bool IsShipped
+        {
+            get { return isShipped; }
+            set { isShipped = value; this.OnPropertyChanged("IsShipped"); }
+        }
+
+
+        public OrderInfo(decimal? orderId, string customerName, string country, string customerId, string shipCity, bool isShipped)
+        {
+            this.OrderID = orderId;
+            this.CustomerName = customerName;
+            this.Country = country;
+            this.CustomerID = customerId;
+            this.ShipCity = shipCity;
+            this.IsShipped = isShipped;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class ViewModel
+    {
+        private ObservableCollection<OrderInfo> orders;
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return orders; }
+            set { orders = value; }
+        }
+
+        public ViewModel()
+        {
+           
+            
+        }
+
+        public ObservableCollection<OrderInfo> Generate()
+        {
+            orders = new ObservableCollection<OrderInfo>();
+            orders.Add(new OrderInfo(1001, "Thomas Hardy", "Germany", "ALFKI", "Berlin", true));
+            orders.Add(new OrderInfo(1002, "Laurence Lebihan", "Mexico", "ANATR", "Mexico", false));
+            orders.Add(new OrderInfo(1003, "Antonio Moreno", "Mexico", "ANTON", "Mexico", true));
+            orders.Add(new OrderInfo(1004, "Thomas Hardy", "UK", "AROUT", "London", true));
+            orders.Add(new OrderInfo(1005, "Christina Berglund", "Sweden", "BERGS", "Lula", false));
+            return orders;
+        }
+    }
+}
